0d6fbc15a0e04c964f6a6f2f0863b58bbd170805
Jenny <jennysjaarda@gmail.com>
2019-12-17 16:22:48 +0100

    remove create.dir outside plan into `make.R`

--- make.R
+++ make.R
@@ -10,7 +10,7 @@
 # Set working directory
 setwd(here::here())
 
-# Load packages
+# Load packages ----
 source("code/packages.R")
 
 # Load functions and plans  ----
@@ -18,8 +18,18 @@ source("code/functions.R")
 source("code/settings.R")
 source("code/plan.R")
 
+# Create output folders  ---
+dir.create(plink_bed_out,showWarnings = F)
+dir.create("data/processed/phenotype_data/GWAS_input", showWarnings = F)
+create_analysis_dirs("analysis/GWAS")
+
 # Run analyses ----
 
 make(pre_impute,parallelism = "future",jobs = 1, console_log_file = "pre_impute_qc.out", template = list(partition = "sgg"))
+
+baseline_gwas_info = define_baseline_inputs(GWAS_input)
+interaction_gwas_info = define_interaction_inputs(GWAS_input)
+subgroup_gwas_info = define_subgroup_inputs(GWAS_input)
+make(pre_impute,parallelism = "future",jobs = 4, console_log_file = "post_impute.out", template = list(partition = "sgg"))
 
 ### download files and impute on Michigan server
