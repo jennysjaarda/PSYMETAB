7084906eb544ca225d440914e561f5d797d78fa0
Jenny <jennysjaarda@gmail.com>
2020-03-10 16:04:51 +0100

    add more plans

--- make.R
+++ make.R
@@ -31,21 +31,30 @@ create_analysis_dirs("analysis/GWAS")
 make(qc_prep, console_log_file = "qc_prep.log", cache_log_file = "cache_log.csv")
 
 make(pre_impute_qc, console_log_file = "pre_impute_qc.log", cache_log_file = "cache_log.csv",
-  parallelism = "clustermq", jobs = 1, template = list(cpus =16, partition = "sgg",
+  parallelism = "clustermq", jobs = 1, template = list(cpus = 16, partition = "sgg",
   log_file = "/data/sgg2/jenny/projects/PSYMETAB/pre_impute_qc_%a_clustermq.out"))
 
 ### download files and impute on Michigan server
 
+make(download_impute, console_log_file = "download_impute.log", cache_log_file = "cache_log.csv",
+  parallelism = "clustermq", jobs = 1, template = list(cpus = 11, partition = "sgg",
+  log_file = "/data/sgg2/jenny/projects/PSYMETAB/download_impute_%a_clustermq.out"))
+
 make(post_impute, console_log_file = "post_impute.log", cache_log_file = "cache_log.csv",
-  parallelism = "clustermq", jobs = 1, template = list(cpus =16, partition = "sgg",
+  parallelism = "clustermq", jobs = 1, template = list(cpus = 16, partition = "sgg",
   log_file = "/data/sgg2/jenny/projects/PSYMETAB/post_impute_%a_clustermq.out"))
 
 make(analysis_prep, console_log_file = "analysis_prep.log", cache_log_file = "cache_log.csv",
-  parallelism = "clustermq", jobs = 4)
+  parallelism = "clustermq", jobs = 4, template = list(cpus = 1, partition = "sgg",
+  log_file = "/data/sgg2/jenny/projects/PSYMETAB/analysis_prep_%a_clustermq.out"))
 
 make(init_analysis, console_log_file = "init_analysis.log", cache_log_file = "cache_log.csv",
-  parallelism = "clustermq", jobs = 3, template = list(cpus =16, partition = "cluster",
+  parallelism = "clustermq", jobs = 3, template = list(cpus = 16, partition = "cluster",
   log_file = "/data/sgg2/jenny/projects/PSYMETAB/init_analysis_%a_clustermq.out"))
+
+make(prs, console_log_file = "prs.log", cache_log_file = "cache_log.csv",
+  parallelism = "clustermq", jobs = 4, template = list(cpus = 16, partition = "sgg",
+  log_file = "/data/sgg2/jenny/projects/PSYMETAB/prs_%a_clustermq.out"))
 
 
 # Now definied within dynamic target
