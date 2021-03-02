cef8cae86aaf11b0edfe5a3487545fcf415b624f
jennysjaarda <jennysjaarda@gmail.com>
2021-03-02 11:45:07 +0100

    modify master extraction script

--- code/extractions/master_extract.sh
+++ code/extractions/master_extract.sh
@@ -1,52 +1,11 @@
-#!/bin/bash
+requestor='FirstnameLastname'
+code_directory=$projects/PSYMETAB/code/extractions
+master_file=$code_directory/master_extract.sh
 
-#SBATCH --job-name=extract_PSYMETAB_FirstnameLastname                # Job name (this name will appear in the queue)
-#SBATCH --chdir=/data/sgg2/jenny/projects/PSYMETAB                 # The Working Directory of the job
-#SBATCH --ntasks=1                                                 # Run on a single core
-#SBATCH --time=24:00:00                                            # Time limit d-hrs:min:sec
-#SBATCH --output=data/processed/extractions/%x.out                 # Standard output and error log (%j: job allocation number)
-#SBATCH --account=sgg                                              # runs on the sggg nodes.
-#SBATCH --partition=sgg                                            # runs on the sggg nodes
+sed -i "s/FirstnameLastname/$requestor/g" "$master_file" > $code_directory/${requestor}_extract_test.sbatch
 
-project_dir="/data/sgg2/jenny/projects/PSYMETAB"
-input=$projects/PSYMETAB/data/raw/extractions/FirstnameLastname
-output=$projects/PSYMETAB/data/processed/extractions/FirstnameLastname
-QC_dir=$project_dir/analysis/QC
-#qc_data=${QC_dir}/15_final_processing/CEU/PSYMETAB_GWAS.CEU
-#pc_data=${QC_dir}/15_final_processing/final_pca/CEU/pcs.PSYMETAB_GWAS_CEU_unrelated.txt
-qc_data=${QC_dir}/15_final_processing/FULL/PSYMETAB_GWAS.FULL
-reference_data=$data/HRC.r1-1.GRCh37.wgs.mac5.sites.tab
-plink_data="PLINK_091019_0920"
-input_chip=$project_dir/data/processed/${plink_data}/PSYMETAB_GWAS
-pc_data=${QC_dir}/15_final_processing/final_pca/ETH/pcs.PSYMETAB_GWAS_ETH_unrelated.txt
+## Copy to terminal:
+#chmod +x  $code_directory/${requestor}_extract_test.sh
+#$code_directory/${requestor}_extract_test.sh
 
-
-if [ ! -d "${output}" ] ; then
-mkdir ${output}
-fi
-
-FILES=$input/*.txt
-
-
-for file in $FILES
-do
-  file_start=$(echo $file | cut -f1 -d.)
-  out_name=$(basename $file_start)
-  if [ ! -d "${output}/${out_name}" ] ; then
-    echo "Processing the following file: "$file
-    mkdir ${output}/${out_name}
-    sh code/extractions/extract_snps.sh $file $output $qc_data $pc_data $QC_dir $input_chip
-    echo "Finished processing file: "$file
-    echo $'\n'
-  else
-    echo "The following SNP list has already been processed: "$file
-    echo "If the code has been modified, delete the folder located here:"
-    echo ${output}/${out_name}
-    echo "and the code will rerun."
-    echo $'\n'
-  fi
-
-done
-
-
-#sbatch $projects/PSYMETAB/code/extractions/FirstnameLastname_extract.sh
+sbatch $projects/PSYMETAB/code/extractions/${requestor}_extract.sbatch
