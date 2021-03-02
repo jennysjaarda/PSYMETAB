9238373408b5982ab80d5f813f7d8c6cc2e5f200
jennysjaarda <jennysjaarda@gmail.com>
2021-03-02 12:19:24 +0100

    test master with aurelie and nicolas

--- code/extractions/master_extract.sh
+++ code/extractions/master_extract.sh
@@ -1,11 +1,31 @@
-requestor='FirstnameLastname'
+#!/bin/bash
+
+#SBATCH --job-name=extract_PSYMETAB                                # Job name (this name will appear in the queue)
+#SBATCH --chdir=/data/sgg2/jenny/projects/PSYMETAB                 # The Working Directory of the job
+#SBATCH --ntasks=1                                                 # Run on a single core
+#SBATCH --time=24:00:00                                            # Time limit d-hrs:min:sec
+#SBATCH --output=data/processed/extractions/%x.out                 # Standard output and error log (%j: job allocation number)
+#SBATCH --account=sgg                                              # runs on the sggg nodes.
+#SBATCH --partition=sgg                                            # runs on the sggg nodes
+
 code_directory=$projects/PSYMETAB/code/extractions
-master_file=$code_directory/master_extract.sh
+master_file=$code_directory/master_extract.sbatch
 
-sed -i "s/FirstnameLastname/$requestor/g" "$master_file" > $code_directory/${requestor}_extract_test.sbatch
+requestor_folders=`ls -d data/raw/extractions/*`
 
-## Copy to terminal:
-#chmod +x  $code_directory/${requestor}_extract_test.sh
-#$code_directory/${requestor}_extract_test.sh
+#for requestor_folder in $requestor_list
+for requestor_folder in NicolasAnsermot AurelieReymond
 
-sbatch $projects/PSYMETAB/code/extractions/${requestor}_extract.sbatch
+do
+
+  requestor=$(basename $requestor_folder)
+  sed -i "s/FirstnameLastname/$requestor/g" "$master_file" > $code_directory/${requestor}_extract.sbatch
+
+  sbatch $projects/PSYMETAB/code/extractions/${requestor}_extract.sh
+
+  wait
+
+done
+
+
+#sbatch $projects/PSYMETAB/code/extractions/master_extract.sh
