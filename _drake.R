15c5b1ebd56b39b39d076b0f04205294b9adcf8d
jennysjaarda <jennysjaarda@gmail.com>
2021-04-26 16:45:51 +0200

    add 'options(clustermq.scheduler = "multicore")'

--- _drake.R
+++ _drake.R
@@ -5,6 +5,8 @@ source("R/functions.R")
 source("R/settings.R")
 source("R/plan.R")
 
+options(clustermq.scheduler = "multicore")
+
 plan <- bind_plans(qc_prep, pre_impute_qc, download_impute, post_impute, analysis_prep, analysis_prep_case_only, init_analysis, process_init)
 
 drake_config(plan, verbose = 2)
