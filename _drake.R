3c47dac384be146d598d43e22463fe3296983802
jennysjaarda <jennysjaarda@gmail.com>
2021-04-27 08:59:01 +0200

    add slurm as option

--- _drake.R
+++ _drake.R
@@ -5,7 +5,7 @@ source("R/functions.R")
 source("R/settings.R")
 source("R/plan.R")
 
-options(clustermq.scheduler = "multicore")
+options(clustermq.scheduler = "slurm", clustermq.template = "slurm_clustermq.tmpl")
 
 plan <- bind_plans(qc_prep, pre_impute_qc, download_impute, post_impute, analysis_prep, analysis_prep_case_only, init_analysis, process_init)
 
