ae30c81aa3ae6cd4ef725217d85f31ba3404bdc9
jennysjaarda <jennysjaarda@gmail.com>
2021-04-27 08:42:42 +0200

    set plan clustermq requirements

--- _drake.R
+++ _drake.R
@@ -9,4 +9,6 @@ options(clustermq.scheduler = "multicore")
 
 plan <- bind_plans(qc_prep, pre_impute_qc, download_impute, post_impute, analysis_prep, analysis_prep_case_only, init_analysis, process_init)
 
-drake_config(plan, verbose = 2)
+drake_config(plan, verbose = 2, log_make = "plan.log", cache_log_file = "cache_log.csv",
+  parallelism = "clustermq", jobs = 120, template = list(cpus = 1, partition = "cluster",
+  log_file = "/data/sgg2/jenny/projects/PSYMETAB/process_init_%a_clustermq.out"))
