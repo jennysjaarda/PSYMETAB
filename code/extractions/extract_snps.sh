e326f478d21c6f7ef22af00197bbdd870a1ea29d
jennysjaarda <jennysjaarda@gmail.com>
2021-05-10 12:16:57 +0200

    extract imputation info from pvar file in SNP extractions

--- code/extractions/extract_snps.sh
+++ code/extractions/extract_snps.sh
@@ -10,20 +10,25 @@ file_start=$(echo $snp_file | cut -f1 -d.)
 out_name=$(basename $file_start)
 
 clean_file=$output/${out_name}/${out_name}_rsids.txt
-snp_info=$output/${out_name}/${out_name}_SNP_info.txt
+snp_HRC_info=$output/${out_name}/${out_name}_SNP_HRC_info.txt
+snp_impute_info=$output/${out_name}/${out_name}_SNP_impute_info.txt
 
   #clean input file
 tr -d '\r' < $snp_file | sed '/^$/d' > $clean_file
 
+# get CHR/BP information from HRC
 num=$(awk 'NR>0 {ORS=" ";print NR}' $clean_file)
-awk 'FNR==NR {a[$1]; next}; $3 in a' $clean_file $data/HRC.r1-1.GRCh37.wgs.mac5.sites.tab > $snp_info
+awk 'FNR==NR {a[$1]; next}; $3 in a' $clean_file $data/HRC.r1-1.GRCh37.wgs.mac5.sites.tab > $snp_hrc_info
+sed  -i '1i #CHROM  POS ID  REF ALT AC  AN  AF  AC_EXCLUDING_1000G  AN_EXCLUDING_1000G  AF_EXCLUDING_1000G  AA' $snp_hrc_info
 
+awk 'FNR==NR {a[$1]; next}; $3 in a' $clean_file $qc_data.pvar > $snp_impute_info
+sed  -i '1i #CHROM  POS  ID REF ALT FILTER  INFO' $snp_impute_info
 
 for chr in {1..22}
 do
 
 
-  awk -v var="$chr " '{ if ($1 == var) { print $3 } }' $snp_info > $output/$out_name/${out_name}_chr${chr}.txt
+  awk -v var="$chr " '{ if ($1 == var) { print $3 } }' $snp_hrc_info > $output/$out_name/${out_name}_chr${chr}.txt
   num_snps=$(cat $output/$out_name/${out_name}_chr${chr}.txt | wc -l)
   if [ ${num_snps} != 0 ]; then
     plink2 --pfile $qc_data --extract $output/${out_name}/${out_name}_chr${chr}.txt --make-pfile --out $output/${out_name}/${out_name}_chr${chr}_extract
